20b785cb4165bfab06f9e92d5aa06ddf9cb2a4b0
ives sun <ives@housetube.tw>
2017-09-30 15:14:34 +0800

    lesson5

--- swoosh/swoosh/ViewController.swift
+++ swoosh/swoosh/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue) {
+        
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
